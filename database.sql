c10adf2d0d31c814940ff0006cb2865789208a55
Parusharam <parusharam4888@gmail.com>
2023-06-20 16:55:08 +0530

    example database

---
+++ database.sql
@@ -0,0 +1,18 @@
+create database Dashboard
+
+-- for login users
+
+CREATE TABLE Users (
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+
+-- for project details
+
+CREATE TABLE projects (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  department VARCHAR(255) NOT NULL,
+  status VARCHAR(20) NOT NULL
+);
