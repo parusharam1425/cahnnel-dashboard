20ec45a0314ccc0ae8debafddb594da441b55765
Parusharam <parusharam4888@gmail.com>
2023-06-22 12:06:22 +0530

    Added to Database

--- src/components/routes/Customers.js
+++ src/components/routes/Customers.js
@@ -1,48 +1,50 @@
-import React, { useState } from 'react';
-import { BsArrowRight, BsPhone, BsChat, BsPerson } from 'react-icons/bs';
+import React, { useState, useEffect } from 'react';
+import { BsArrowRight, BsPhone, BsChat, BsPerson, BsTrash } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import './styles/Customer.css';
 
 import RImage from './assects/image-user.jpg';
 
-const customersData = [
-  {
-    id: 1,
-    name: 'Yadav',
-    title: 'CEO Expert',
-    imgSrc: RImage,
-    age: 30,
-    email: 'yadav@example.com',
-    phone: '1234567890',
-    address: '123 Main St, City',
-  },
-  {
-    id: 2,
-    name: 'swathi',
-    title: 'BTS Expert',
-    imgSrc: 'imh.jpg',
-    age: 30,
-    email: 'yadav@example.com',
-    phone: '1234567890',
-    address: '123 Main St, City',
-  },
-];
-
-function Customers() {
+const Customers = () => {
   const [showAll, setShowAll] = useState(false);
   const [showAddModal, setShowAddModal] = useState(false);
-  const [newCustomer, setNewCustomer] = useState({
-    id: null,
-    name: '',
-    title: '',
-    imgSrc: '',
-    age: null,
-    email: '',
-    phone: '',
-    address: '',
+  const [newCustomer, setNewCustomer] = useState(() => {
+    const savedCustomer = localStorage.getItem('newCustomer');
+    return savedCustomer
+      ? JSON.parse(savedCustomer)
+      : {
+          id: null,
+          name: '',
+          title: '',
+          imgSrc: '',
+          age: null,
+          email: '',
+          phone: '',
+          address: '',
+        };
   });
-  const [customerList, setCustomerList] = useState(customersData);
+  const [customerList, setCustomerList] = useState([]);
+
+  useEffect(() => {
+    // Fetch customer data from the API
+    axios
+      .get('http://localhost:3001/customers')
+      .then((response) => {
+        // Customer data fetched successfully
+        setCustomerList(response.data);
+      })
+      .catch((error) => {
+        // Error occurred while fetching customer data
+        console.error('Error fetching customer data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Save newCustomer data to local storage
+    localStorage.setItem('newCustomer', JSON.stringify(newCustomer));
+  }, [newCustomer]);
 
   const displayedCustomers = showAll ? customerList : customerList.slice(0, 2);
 
@@ -59,31 +61,71 @@ function Customers() {
     setNewCustomer({ ...newCustomer, [name]: value });
   };
 
-  const addCustomer = () => {
-    const newId = customerList.length + 1;
-    const newCustomerData = { ...newCustomer, id: newId };
-    setCustomerList([...customerList, newCustomerData]);
-    setNewCustomer({
-      id: null,
-      name: '',
-      title: '',
-      imgSrc: '',
-      age: null,
-      email: '',
-      phone: '',
-      address: '',
-    });
-    toggleAddModal();
-  };
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
-    const reader = new FileReader();
+    const blob = new Blob([file], { type: file.type });
+    setNewCustomer((prevState) => ({ ...prevState, imgSrc: blob }));
+  };
 
-    reader.onload = (event) => {
-      setNewCustomer((prevState) => ({ ...prevState, imgSrc: event.target.result }));
-    };
+  const addCustomer = () => {
+    // Create a FormData object to send customer data and image separately
+    const formData = new FormData();
+    formData.append('name', newCustomer.name);
+    formData.append('title', newCustomer.title);
+    formData.append('age', newCustomer.age);
+    formData.append('email', newCustomer.email);
+    formData.append('phone', newCustomer.phone);
+    formData.append('address', newCustomer.address);
+    formData.append('image', newCustomer.imgSrc);
 
-    reader.readAsDataURL(file);
+    axios
+      .post('http://localhost:3001/customers', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        // Customer data added successfully
+        const addedCustomer = {
+          id: response.data.id,
+          name: newCustomer.name,
+          title: newCustomer.title,
+          imgSrc: URL.createObjectURL(newCustomer.imgSrc),
+          age: newCustomer.age,
+          email: newCustomer.email,
+          phone: newCustomer.phone,
+          address: newCustomer.address,
+        };
+        setCustomerList([...customerList, addedCustomer]);
+        setNewCustomer({
+          id: null,
+          name: '',
+          title: '',
+          imgSrc: '',
+          age: null,
+          email: '',
+          phone: '',
+          address: '',
+        });
+        toggleAddModal();
+      })
+      .catch((error) => {
+        // Error occurred while adding customer data
+        console.error('Error adding customer data:', error);
+      });
+  };
+
+  const deleteCustomer = (id) => {
+    axios
+      .delete(`http://localhost:3001/customers/${id}`)
+      .then(() => {
+        // Customer data deleted successfully
+        setCustomerList(customerList.filter((customer) => customer.id !== id));
+      })
+      .catch((error) => {
+        // Error occurred while deleting customer data
+        console.error('Error deleting customer data:', error);
+      });
   };
 
   return (
@@ -119,38 +161,80 @@ function Customers() {
                 <span className="action-icon">
                   <BsPhone />
                 </span>
+                <span className="action-icon text-danger" onClick={() => deleteCustomer(customer.id)}>
+                  <BsTrash />
+                </span>
               </div>
             </div>
           ))}
         </div>
         <div className="add-customer">
-        <button className='add-button' onClick={toggleAddModal}>Add Customer</button>
+          <button className="add-button" onClick={toggleAddModal}>
+            Add Customer
+          </button>
+        </div>
       </div>
-      </div>
-
-      
 
       {showAddModal && (
-       <div className="modal">
-       <div className="modal-content">
-         <h2>Add Customer</h2>
-         <input type="text" name="name" placeholder="Name" value={newCustomer.name} onChange={handleInputChange} />
-         <input type="text" name="title" placeholder="Title" value={newCustomer.title} onChange={handleInputChange} />
-         <input type="file" name="image" onChange={handleImageUpload} />
-         <input type="number" name="age" placeholder="Age" value={newCustomer.age} onChange={handleInputChange} />
-         <input type="email" name="email" placeholder="Email" value={newCustomer.email} onChange={handleInputChange} />
-         <input type="tel" name="phone" placeholder="Phone" value={newCustomer.phone} onChange={handleInputChange} />
-         <input type="text" name="address" placeholder="Address" value={newCustomer.address} onChange={handleInputChange} />
-         <div className="modal-actions">
-           <button className='btn btn-primary' onClick={addCustomer}>Add</button>
-           <button className='btn btn-secondary' onClick={toggleAddModal}>Cancel</button>
-         </div>
-       </div>
-     </div>
-     
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Add Customer</h2>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={newCustomer.name}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={newCustomer.title}
+              onChange={handleInputChange}
+            />
+            <input type="file" name="image" onChange={handleImageUpload} />
+            <input
+              type="number"
+              name="age"
+              placeholder="Age"
+              value={newCustomer.age}
+              onChange={handleInputChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={newCustomer.email}
+              onChange={handleInputChange}
+            />
+            <input
+              type="tel"
+              name="phone"
+              placeholder="Phone"
+              value={newCustomer.phone}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="address"
+              placeholder="Address"
+              value={newCustomer.address}
+              onChange={handleInputChange}
+            />
+            <div className="modal-actions">
+              <button className="btn btn-primary" onClick={addCustomer}>
+                Add
+              </button>
+              <button className="btn btn-secondary" onClick={toggleAddModal}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
-}
+};
 
 export default Customers;
